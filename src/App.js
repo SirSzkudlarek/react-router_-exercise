55b5cd7ec6a94fe49a43bcc7cd75d4e2d8e06bbf
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-05-26 00:38:29 +0200

    Learning Route:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => {
+  return <h1>Homepage</h1>;
+};
+
+const News = () => <h1>News</h1>;
+const Contact = () => <h1>Contact</h1>;
+const Error = () => <h1>404 not found...</h1>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/" exact="true">
+                    Start
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/news">News</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </section>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
